1e11deb1eae599d5124fc059800b30df1f8c1daa
asan_mohamed <118803971+Asanmohamed123@users.noreply.github.com>
2025-11-30 10:44:39 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
-from flask import *  
+import os
+import uuid
+from flask import Flask, flash, request, redirect
 import speech_recognition as sr
-import pyttsx3
 
-app = Flask(__name__)  
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
- 
-@app.route('/') 
-def home():
+UPLOAD_FOLDER = 'files'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/save-record', methods=['POST'])
+def save_record():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    file_name = str(uuid.uuid4()) + ".mp3"
+    output_file = 'user_recording.wav'
+    file.save(output_file)
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        speak(query)
-    except Exception as e:
-        query = "Say that again please..."
-        speak(query)
-    return render_template('message.html',query=query)  
-  
-if __name__ =='__main__':  
-    app.run(debug = True)  
+    with sr.AudioFile(output_file) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data, language='en-IN', show_all=True)
+    print(text)
+    return '<h1>Success</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
